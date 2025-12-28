1ae97f71743109ca19bfbbca977a9fef900c16ea
Khyta <khyta@pm.me>
2025-12-28 16:06:40 +0100

    feat: create first successful API call

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+)
+
+var apiEndpoint string = "https://38c3.c3nav.de/api/v2/"
 
 func main() {
-	fmt.Println("Hello World")
+
+	statusAPI := apiEndpoint + "auth/status"
+
+	resp, err := http.Get(statusAPI)
+	if err != nil {
+		slog.Error("status check unsuccessfull.", "error", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	fmt.Println(string(body[:]))
 }
