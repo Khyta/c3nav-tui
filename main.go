1bb0c9e39ff67ce1b49523424d05087e68e74e3a
Khyta <khyta@pm.me>
2025-12-28 18:29:37 +0100

    feat: store session key in struct

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
@@ -9,7 +10,38 @@ import (
 
 var apiEndpoint string = "https://39c3.c3nav.de/api/v2/"
 
+// TODO: attach method to struct to store session key data
+type sessionKey struct {
+	Key string `json:"key"`
+}
+
+func (k *sessionKey) getSessionKey() {
+	sessionURL := apiEndpoint + "auth/session/"
+	resp, err := http.Get(sessionURL)
+	if err != nil {
+		slog.Error("response broken", "error", err)
+		return
+	}
+	slog.Info("initial response.", "status", resp.Status)
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		slog.Error("could not read response.", "error", err)
+		return
+	}
+
+	err = json.Unmarshal(body, &k)
+	if err != nil {
+		slog.Error("could not unmarshal body json.", "error", err)
+	}
+}
+
 func main() {
+
+	var s sessionKey
+	s.getSessionKey()
+	slog.Info("got session key.", "key", s.Key)
 
 	statusAPI := apiEndpoint + "auth/status"
 
